5ffcbd2f1cf48e0580a5e0ebfae8adf97f524073
ZPyAo <121719459+LilYau350@users.noreply.github.com>
2025-02-23 02:59:45 +0800

    Update KOALA.py

--- classification_cifar100/optimizers/KOALA.py
+++ classification_cifar100/optimizers/KOALA.py
@@ -1,26 +1,5 @@
 import torch
 
-#from util import ExpAverage
-
-class AverageMeter(object):
-    def __init__(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
-    def get_avg(self):
-        return self.avg
-
-    def get_last_val(self):
-        return self.val
-
 
 class ExpAverage(object):
     def __init__(self, alpha, init_val=0):
