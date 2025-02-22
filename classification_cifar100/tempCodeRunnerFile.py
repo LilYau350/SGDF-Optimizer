1e8b091648be43584c461646b5b76807b56859e9
ZPyAo <121719459+LilYau350@users.noreply.github.com>
2025-02-23 03:31:31 +0800

    Delete classification_cifar100/tempCodeRunnerFile.py

--- classification_cifar100/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-                with open(gradient_file_path, 'a') as f:
-                    writer = csv.writer(f)
-                    for gradient_tensor in gradients:
-                        writer.writerow(gradient_tensor.cpu().numpy())
