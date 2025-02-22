5861eb5b27d26f1183a2916d97b75c1e0756d554
ZPyAo <121719459+LilYau350@users.noreply.github.com>
2025-02-23 03:38:51 +0800

    Update run.sh

--- classification_cifar100/run.sh
+++ classification_cifar100/run.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python main.py --optim sgdf --lr 0.3 --eps 1e-8 --beta1 0.9 --beta2 0.999 --momentum 0.9
+CUDA_VISIBLE_DEVICES=0 python main.py --optim sgdf --lr 0.5 --eps 1e-8 --beta1 0.9 --beta2 0.999 --润0
