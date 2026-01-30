218a0e15dc1dc8a3698b3161c598b2ffd8e064ec
ZPyAo <121719459+LilYau350@users.noreply.github.com>
2026-01-30 22:34:58 +0800

    Add files via upload

---
+++ Muon.py
@@ -0,0 +1,133 @@
+import torch
+from torch.optim.optimizer import Optimizer
+
+
+def zeropower_via_newtonschulz5(G, steps: int):
+    """
+    Newton-Schulz iteration to compute the zeroth power / orthogonalization of G. We opt to use a
+    quintic iteration whose coefficients are selected to maximize the slope at zero. For the purpose
+    of minimizing steps, it turns out to be empirically effective to keep increasing the slope at
+    zero even beyond the point where the iteration no longer converges all the way to one everywhere
+    on the interval. This iteration therefore does not produce UV^T but rather something like US'V^T
+    where S' is diagonal with S_{ii}' ~ Uniform(0.5, 1.5), which turns out not to hurt model
+    performance at all relative to UV^T, where USV^T = G is the SVD.
+    """
+    assert G.ndim >= 2 # batched Muon implementation by @scottjmaddox, and put into practice in the record by @YouJiacheng
+    a, b, c = (3.4445, -4.7750,  2.0315)
+    X = G.bfloat16()
+    if G.size(-2) > G.size(-1):
+        X = X.mT
+
+    # Ensure spectral norm is at most 1
+    X = X / (X.norm(dim=(-2, -1), keepdim=True) + 1e-7)
+    # Perform the NS iterations
+    for _ in range(steps):
+        A = X @ X.mT
+        B = b * A + c * A @ A # quintic computation strategy adapted from suggestion by @jxbz, @leloykun, and @YouJiacheng
+        X = a * X + B @ X
+    
+    if G.size(-2) > G.size(-1):
+        X = X.mT
+    return X
+
+
+def muon_update(grad, momentum, beta=0.95, ns_steps=5, nesterov=True):
+    momentum.lerp_(grad, 1 - beta)
+    update = grad.lerp_(momentum, beta) if nesterov else momentum
+    if update.ndim == 4: # for the case of conv filters
+        update = update.view(len(update), -1)
+    update = zeropower_via_newtonschulz5(update, steps=ns_steps)
+    # update *= max(1, grad.size(-2) / grad.size(-1))**0.5
+    update *= 0.2 * max(grad.size(-2), grad.size(-1))**0.5
+    return update
+
+
+class Muon(torch.optim.Optimizer):
+    """
+    Muon - MomentUm Orthogonalized by Newton-schulz
+
+    https://kellerjordan.github.io/posts/muon/
+
+    Muon internally runs standard SGD-momentum, and then performs an orthogonalization post-
+    processing step, in which each 2D parameter's update is replaced with the nearest orthogonal
+    matrix. For efficient orthogonalization we use a Newton-Schulz iteration, which has the
+    advantage that it can be stably run in bfloat16 on the GPU.
+
+    Muon should only be used for hidden weight layers. The input embedding, final output layer,
+    and any internal gains or biases should be optimized using a standard method such as AdamW.
+    Hidden convolutional weights can be trained using Muon by viewing them as 2D and then
+    collapsing their last 3 dimensions.
+
+    Arguments:
+        lr: The learning rate, in units of spectral norm per update.
+        weight_decay: The AdamW-style weight decay.
+        momentum: The momentum. A value of 0.95 here is usually fine.
+    """
+    def __init__(self, params, lr=0.02, weight_decay=0.0, beta=0.95, nesterov=False, ns_steps=5):
+        if lr <= 0:
+            raise ValueError(f"Invalid lr: {lr}")
+        if not (0.0 <= weight_decay):
+            raise ValueError(f"Invalid weight_decay: {weight_decay}")
+        if not (0.0 < beta < 1.0):
+            raise ValueError(f"Invalid momentum(beta): {beta}")
+        if ns_steps < 1:
+            raise ValueError(f"Invalid ns_steps: {ns_steps}")
+
+        defaults = dict(
+            lr=lr,
+            weight_decay=weight_decay,
+            beta=beta,
+            nesterov=nesterov,
+            ns_steps=ns_steps,
+        )
+        super().__init__(params, defaults)
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            lr = group["lr"]
+            weight_decay = group["weight_decay"]
+            beta = group["beta"]
+            nesterov = group["nesterov"]
+            ns_steps = group["ns_steps"]
+
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                
+                if p.grad.is_sparse:
+                    raise RuntimeError("Muon does not support sparse gradients.")
+
+                if p.ndim < 2:
+                    continue
+
+                grad = p.grad
+                
+                # decoupled weight decay 
+                if weight_decay != 0.0:
+                    p.mul_(1 - lr * weight_decay)
+                    
+                state = self.state[p]
+                if len(state) == 0:
+                    state["momentum_buffer"] = torch.zeros_like(p)
+
+                momentum = state["momentum_buffer"]
+
+                # compute muon update (same shape as p)
+                update = muon_update(
+                    grad=grad,
+                    momentum=momentum,
+                    beta=beta,
+                    ns_steps=ns_steps,
+                    nesterov=nesterov,
+                )
+
+                # gradient step
+                p.add_(update, alpha=-lr)
+
+        return loss
